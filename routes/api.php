19972c6d104dd00ad9edb4d327417765f55d3838
vybukhivka <viktorbakhovka@gmail.com>
2025-10-28 15:11:45 +0200

    ProjectController + ProjectResources, updated bootstrap/app (api route), api.php with protected projects route

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\ProjectController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('projects', ProjectController::class);
+});
