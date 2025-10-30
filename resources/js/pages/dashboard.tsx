d04c8e5469b8c5a414f5bb7f0cd2dc2d35dcb2e4
vybukhivka <viktorbakhovka@gmail.com>
2025-10-30 14:02:36 +0200

    projects listed on dashboard page

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Projects',
         href: dashboard().url,
     },
 ];
@@ -50,17 +50,53 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
+                    {isLoading && <div>Loading...</div>}
+                    {isError && <div>Error fetching data.</div>}
+                    {projects &&
+                        projects.map((project) => (
+                            <div
+                                key={project.id}
+                                className="relative flex aspect-video flex-col justify-between overflow-hidden rounded-xl border border-sidebar-border/70 p-4 shadow-sm dark:border-sidebar-border"
+                            >
+                                <div className="mb-3 flex justify-between">
+                                    <h3 className="mb-3 text-lg font-semibold text-gray-900 dark:text-white">
+                                        <span>{project.title}</span>
+                                    </h3>
+                                    <span
+                                        className={`relative top-3 inline-block h-1 w-1 rounded-full ${project.is_done ? 'bg-green-500' : 'bg-red-500'} `}
+                                    ></span>
+                                </div>
+
+                                <div className="space-y-2 text-sm">
+                                    <div className="flext justify-between">
+                                        <span className="text-gray-500 dark:text-gray-400">
+                                            Genre:{' '}
+                                        </span>
+                                        <span className="font-medium text-gray-700 dark:text-gray-300">
+                                            {project.genre}
+                                        </span>
+                                    </div>
+                                    <div className="flext justify-between">
+                                        <span className="text-gray-500 dark:text-gray-400">
+                                            Tracks:{' '}
+                                        </span>
+                                        <span className="font-medium text-gray-700 dark:text-gray-300">
+                                            {project.number_of_tracks}
+                                        </span>
+                                    </div>
+                                    <div className="flext justify-between">
+                                        <span className="text-gray-500 dark:text-gray-400">
+                                            Duration:{' '}
+                                        </span>
+                                        <span className="font-medium text-gray-700 dark:text-gray-300">
+                                            {project.duration_minutes}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                <div className="min-h- relative flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                     <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                 </div>
             </div>
