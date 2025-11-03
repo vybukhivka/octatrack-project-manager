f18b193f402ad973be2f999723da8b6eee8c71a6
vybukhivka <viktorbakhovka@gmail.com>
2025-11-03 16:01:37 +0200

    ProjectController validation + DB commit

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectResource;
 use App\Jobs\ProcessProjectBackup;
@@ -31,14 +32,20 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): ProjectResource
+    public function store(Request $request): JsonResponse
     {
         $validated = $request->validate([
             'title' => 'required|string|max:32',
             'genre' => 'nullable|string|max:16',
         ]);
 
-        $project = $request->user()->projects()->create($validated);
+        $data = array_merge($validated, [
+            'number_of_tracks' => 8,
+            'duration_minutes' => 0,
+            'is_done' => false,
+        ]);
+
+        $project = $request->user()->projects()->create($data);
 
         TrackLayout::factory(8)->create(['project_id' => $project->id]);
         PartLayout::factory(4)->create(['project_id' => $project->id]);
@@ -65,15 +72,60 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project): ProjectResource
     {
-        if ($request->user()->id !== $project->user_id) {
+        if ($project->user_id !== $request->user()->id) {
             abort(403);
         }
 
         $validated = $request->validate([
             'title' => 'required|string|max:32',
             'genre' => 'nullable|string|max:16',
+            'tracks' => 'required|array',
+            'parts' => 'required|array',
+            'scenes' => 'required|array',
+            'tracks.*.id' => 'required|integer|exists:track_layouts,id',
+            'tracks.*.label' => 'nullable|string|max:32',
+            'parts.*.id' => 'required|integer|exists:part_layouts,id',
+            'parts.*.label' => 'nullable|string|max:32',
+            'scenes.*.id' => 'required|integer|exists:scene_layouts,id',
+            'scenes.*.label' => 'nullable|string|max:32',
         ]);
-        $project->update($validated);
+
+        try {
+            DB::beginTransaction();
+
+            $project->update([
+                'title' => $validated['title'],
+                'genre' => $validated['genre'],
+            ]);
+
+            foreach ($validated['tracks'] as $trackData) {
+                TrackLayout::where('id', $trackData['id'])
+                    ->where('project_id', $project->id)
+                    ->update(['label' => $trackData['label']]);
+            }
+
+            foreach ($validated['parts'] as $partData) {
+                PartLayout::where('id', $partData['id'])
+                    ->where('project_id', $project->id)
+                    ->update(['label' => $partData['label']]);
+            }
+
+            foreach ($validated['scenes'] as $sceneData) {
+                SceneLayout::where('id', $sceneData['id'])
+                    ->where('project_id', $project->id)
+                    ->update(['label' => $sceneData['label']]);
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(
+                ['message' => 'Update failed', 'error' => $e->getMessage()],
+                500,
+            );
+        }
+
+        $project->load(['trackLayout', 'partLayout', 'sceneLayout']);
 
         return new ProjectResource($project);
     }
